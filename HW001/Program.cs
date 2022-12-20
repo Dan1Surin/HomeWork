cfb6c2ffd40d5d889dcd34c4975db747a471c72d
Даниил Сурин <dan1surin@gmail.com>
2022-12-20 10:53:50 +0300

    add HW01

--- HW001/Program.cs
+++ HW001/Program.cs
@@ -3,11 +3,13 @@ Console.Write("a = ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("b = ");
 int b = Convert.ToInt32(Console.ReadLine());
+if (a == b) Console.WriteLine("Числа равны"); //добавил на равность чисел
 if (a > b)
 {
-    Console.Write($"max = {a}"); 
+    Console.Write($"max = {a}");
 }
 else
 {
-    Console.Write($"max = {b}"); 
+    Console.Write($"max = {b}");
 }
+Console.ReadLine();
